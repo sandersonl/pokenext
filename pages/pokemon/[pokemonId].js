58dde32e17859400fe12d431de4b61ab2515e8cf
Lucas Sanderson <lucassanderson.ls@gmail.com>
2022-05-24 09:54:42 -0300

    update of rendering

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,5 +1,6 @@
 
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import styles from '../../styles/Pokemon.module.css'
 
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     }
 }
 
@@ -39,6 +40,13 @@ export const getStaticProps = async (context) => {
 }
 
 export default function Pokemon({pokemon}) {
+
+    const router = useRouter()
+
+    if(router.isFallback) {
+        return <div>Carregando...</div>
+    }
+
     return (
         <div className={styles.pokemon_container}>
             <h1 className={styles.title}>{pokemon.name}</h1>
